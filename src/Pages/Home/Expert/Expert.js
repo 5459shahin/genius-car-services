f8d83258543d185cead4b54124b59e4195acfbf9
545n shahin <mohammadshahinkhan1992@gmail.com>
2022-04-15 02:42:36 +0800

    adedd navber

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -4,12 +4,14 @@ const Expert = ({ expert }) => {
     const { name, img } = expert;
 
     return (
-        <div className="card gx-5 gy-5 col-sm-12 col-md-6 col-lg4" style={{ width: "18rem" }}>
-            <img src={img} class="card-img-top" alt="..." />
-            <div class="card-body">
-                <h5 class="card-title">{name}</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        <div className='g-5  col-sm-12 col-md-6 col-lg4'>
+            <div className="card" style={{ width: "18rem" }}>
+                <img src={img} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <a href="#" className='btn btn-primary'>Go SomeWhere</a>
+                </div>
             </div>
         </div>
     );
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -4,21 +4,23 @@ import './Services.css'
 
 const Services = () => {
     const [services, setServices] = useState([]);
-    useEffect( () => {
+    useEffect(() => {
         fetch('services.json')
-        .then(res => res.json())
-        .then(data => setServices(data));
-    },[])
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
     return (
-        <div>
-            <h1 className='services-title'>Our Services</h1>
-            <div className='services-container'>
-            {
-                services.map(service => <Service
-                key={service.id}
-                service={service}
-                ></Service>)
-            }
+        <div className='container'>
+            <div className="row">
+                <h1 className='services-title mt-5 mb-5'>Our Services</h1>
+                <div className='services-container'>
+                    {
+                        services.map(service => <Service
+                            key={service.id}
+                            service={service}
+                        ></Service>)
+                    }
+                </div>
             </div>
         </div>
     );
